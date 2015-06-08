c93e18c958f3f45d1dd8ffb880a5d25fd01c5129
Jack Wright <jackw@ooyala.com>
2015-06-08 10:18:36 +0100

    Change of tack. Decided to make this a tamper monkey script instead, as development will likely be much more rapid.

--- addbutton.js
+++ addbutton.js
@@ -1,8 +1,30 @@
-var bottomRow = document.getElementById("bottomButtonRow");
-var newButton = document.createElement("input"); 
+// ==UserScript==
+// @name         Salesforce Google Calendar
+// @namespace    http://com.ooyala.jackw
+// @version      0.1
+// @description  Adds UI for creating Google Calendar events from Salesforce comments
+// @author       jackw@ooyala.com
+// @match        https://na5.salesforce.com/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// ==/UserScript==
 
-newButton.setAttribute("type","button");
-newButton.setAttribute("value","Set Reminder");
-newButton.className = "btn";
+function addButton(){
+	var topRow = $("#topButtonRow");
+	var bottomRow = $("#bottomButtonRow");
+	var newButtonTop = document.createElement("input"); 
 
-bottomRow.appendChild(newButton);
+	newButtonTop.setAttribute("type","button");
+	newButtonTop.setAttribute("value","Set Reminder");
+	newButtonTop.setAttribute("onclick","alert('Not yet implemented.');");
+	newButtonTop.className = "btn";
+
+	// TODO: Necessary wireup for GCal event creation.
+
+	var newButtonBottom = newButtonTop.cloneNode();
+
+	topRow.append(newButtonTop);
+	bottomRow.append(newButtonBottom);
+}
+
+addButton();
